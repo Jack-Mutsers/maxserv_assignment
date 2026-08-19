6654dfcc7a77a47a0e6ee7841b3c87753a63cddb
Jack mutsers <jmutsers95@hotmail.com>
2026-08-19 14:12:07 +0200

    resolved routing issues

--- Dockerfile
+++ Dockerfile
@@ -36,3 +36,7 @@ ENV APACHE_DOCUMENT_ROOT=/var/www/html/public
 # Update the apache configuration using the `APACHE_DOCUMENT_ROOT` environment variable
 RUN sed -ri -e 's!/var/www/html!${APACHE_DOCUMENT_ROOT}!g' /etc/apache2/sites-available/*.conf
 RUN sed -ri -e 's!/var/www/html!${APACHE_DOCUMENT_ROOT}!g' /etc/apache2/apache2.conf /etc/apache2/conf-available/*.conf
+
+# Route application URLs through public/index.php.
+RUN a2enmod rewrite && \
+    sed -ri 's/AllowOverride None/AllowOverride All/g' /etc/apache2/apache2.conf
--- packages/core/src/Routing/Router.php
+++ packages/core/src/Routing/Router.php
@@ -23,6 +23,47 @@ readonly class Router
 
         [$controllerClass, $action] = explode('::', $parameters['_controller']);
         $controller = $this->container->get($controllerClass);
-        $controller->$action($parameters);
+
+        $properties = array_filter($parameters, fn($key) => $key[0] !== '_', ARRAY_FILTER_USE_KEY);
+
+        $arguments = $this->getArguments($controller, $action, $properties);
+
+        $controller->$action(...$arguments);
+    }
+
+    private function getArguments($controller, $action, $properties): array
+    {
+        $reflection = new \ReflectionMethod($controller, $action);
+
+        $parameters = [];
+
+        foreach ($reflection->getParameters() as $parameter) {
+            $parameters[$parameter->getName()] = $parameter;
+        }
+
+        $arguments = [];
+
+        foreach ($properties as $name => $value) {
+            if (!isset($parameters[$name])) {
+                continue;
+            }
+
+            $parameter = $parameters[$name];
+            $type = $parameter->getType();
+
+            if ($type instanceof \ReflectionNamedType && $type->isBuiltin()) {
+                $value = match ($type->getName()) {
+                    'int' => (int) $value,
+                    'float' => (float) $value,
+                    'bool' => filter_var($value, FILTER_VALIDATE_BOOL),
+                    'string' => (string) $value,
+                    default => $value,
+                };
+            }
+
+            $arguments[$name] = $value;
+        }
+
+        return $arguments;
     }
 }
